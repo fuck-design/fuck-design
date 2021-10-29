50ae042d73385349d9dfa684b64dbe43180a2fee
hustcc <i@hust.cc>
2021-10-29 20:01:26 +0800

    feat(fuck): button component

---
+++ __tests__/fuck/button.spec.tsx
@@ -0,0 +1,7 @@
+import { Button } from '../../src';
+
+describe('button', () => {
+  test('button', () => {
+    expect(Button).toBeDefined();
+  });
+});
---
+++ src/fuck/button/types.ts
@@ -0,0 +1,10 @@
+/**
+ * Button 的类型定义
+ */
+export type ButtonProps = {
+  /**
+   * button 按钮的文本内容
+   */
+  text: string;
+  onClick?: (e) => void;
+}

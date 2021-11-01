4b29d9ce8e6b0724878002c306ff53001e0da8b8
visiky <xinming.lxj@antgroup.com>
2021-11-01 10:18:57 +0800

    feat(funck): hr component

---
+++ src/fuck/hr/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HrProps } from './types';
+
+export { HrProps };
+
+/**
+ * Pre 组件
+ * @param props
+ * @returns
+ */
+export const Hr: React.FC<HrProps> = (props: HrProps) => {
+
+  return <hr />;
+};
